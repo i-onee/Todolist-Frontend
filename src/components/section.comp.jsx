2bc2bda252e346e3d23bb1d1a123a57b2def16e5
i-one7 <ridhoang93@gmail.com>
2022-09-20 23:06:45 +0700

    change some properties

--- src/components/section.comp.jsx
+++ src/components/section.comp.jsx
@@ -1,14 +1,17 @@
 import { Container, Stack } from '@chakra-ui/react';
 
 const Sections = (props) => {
-	const { children, custom } = props;
+	const { children, container, custom } = props;
 	return (
 		<Stack
 			position={'relative'}
 			display={'block'}
 			{...custom}
 		>
-			<Container maxW={'container.lg'} position={'relative'} display={'block'}>
+			<Container
+				maxW={'container.lg'}
+				position={'relative'}
+				{...container}>
 				{children}
 			</Container>
 		</Stack>

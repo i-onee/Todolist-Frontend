6105ac999ed2b4ec51e82a128239037dd11dd06f
i-one7 <ridhoang93@gmail.com>
2022-09-22 00:32:31 +0700

    remove index.css import

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import ReactDOM from 'react-dom/client';
 import React from 'react';
-import './css/index.css';
 import App from './app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

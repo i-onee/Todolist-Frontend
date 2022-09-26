288eda3055667db8730b0abe56b175557319e01f
i-one7 <ridhoang93@gmail.com>
2022-09-27 02:01:07 +0700

    add some custom colors

--- src/theme/chakra.theme.jsx
+++ src/theme/chakra.theme.jsx
@@ -5,7 +5,7 @@ const styles = {
 	global: (props) => ({
 		body: {
 			background: mode('light.400', 'dark.800')(props),
-			color: mode('gray.900', 'white')(props),
+			color: mode('gray.600', 'gray.400')(props),
 			transitionProperty: 'background',
 			fontFamily: 'SF Pro UI',
 			transitionDuration: '0.2s',
@@ -16,6 +16,7 @@ const styles = {
 	}),
 };
 
+// custom color
 const dark = {
 	700: '#1c1f24',
 	800: '#121417',
@@ -28,10 +29,57 @@ const light = {
 	400: '#f5f7fc'
 };
 
-const indigo = {
-	200: '#8a4af3'
+const leaf = {
+	100: '#8ab754',
+	200: '#8bc34a',
+	300: '#7daf42',
 };
 
+const leafAlpha = {
+	200: `${leaf[ 200 ]}33`
+};
+
+const indigo = {
+	100: '#b892f7',
+	200: '#8a4af3',
+	300: '#673ab7'
+};
+
+const indigoAlpha = {
+	200: `${indigo[200]}33`,
+};
+
+const violet = {
+	100: '#eb3472',
+	200: '#e91e63',
+	300: '#d11b59'
+};
+
+const violetAlpha = {
+	200: `${violet[ 200 ]}33`
+};
+
+const lightBlue = {
+	100: '#03a9f4',
+	200: '#2196f3',
+	300: '#1d87da',
+};
+
+const lightBlueAlpha = {
+	200: `${lightBlue[ 200 ]}33`
+};
+
+const sun = {
+	100: '#ffc71f',
+	200: '#ffc107',
+	300: '#e5ad06',
+};
+
+const sunAlpha = {
+	200: `${sun[ 200 ]}33`,
+};
+
+// custom component
 const Button = {
 	baseStyle: {
 		fontWeight: 'normal',
@@ -41,7 +89,7 @@ const Button = {
 		mr: 0
 	},
 	sizes: {
-		md: { h: 0, w: 0, px: 0, py: 0 }
+		md: { minW: 'fit-content', w: 0, h: 0, px: 0, py: 0 }
 	},
 	variants: {
 		solid: () => ({
@@ -71,7 +119,7 @@ const Textarea = {
 	variants: {
 		outline: (props) => ({
 			bg: mode('light.300', 'blackAlpha.500')(props),
-			height: '11rem',
+			height: '12rem',
 			resize: 'none',
 			border: 0,
 			_focus: {
@@ -91,10 +139,13 @@ const Divider = {
 	},
 };
 
+
+
+// declare theme customs
 const theme = extendTheme({
+	colors: { light, sun, sunAlpha, dark, indigo, indigoAlpha, violet, violetAlpha, lightBlue, lightBlueAlpha, leaf, leafAlpha},
 	components: {Button, Input, Textarea, Divider },
 	config: {disableTransitionOnChange: false},
-	colors : {light, dark, indigo},
 	styles,
 });
 

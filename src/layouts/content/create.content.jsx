2de4564b75492b785f08cf2871dbeb8109aa39b7
i-one7 <ridhoang93@gmail.com>
2022-09-27 04:29:52 +0700

    remove some redundant elements

--- src/layouts/content/create.content.jsx
+++ src/layouts/content/create.content.jsx
@@ -1,7 +1,7 @@
 import { WidgetBody, WidgetHeader, WidgetFooter } from '../../components/widget.comp';
 import { FormContainer, Inputs, TextAreas } from '../../components/form.comp';
 import { Buttons, IButtons } from '../../components/button.comp';
-import { Heading, HStack, Divider } from '@chakra-ui/react';
+import { Heading, HStack, VStack } from '@chakra-ui/react';
 import { EventContext } from '../../context/event.context';
 import { FiLayers } from 'react-icons/fi';
 import { useContext } from 'react';
@@ -9,40 +9,47 @@ import { useContext } from 'react';
 const Createlist = () => {
 	const { setTabs, handleSubmit, titleRef, notesRef } = useContext(EventContext);
 
-
 	return (
-		<>
+		<VStack w={'full'} gap={3}>
 			<WidgetHeader>
 				<HStack alignItems={'center'} justifyContent={'space-between'}>
 					<Heading size={'md'} fontWeight={'medium'}>create task</Heading>
 					<IButtons
 						custom={{
 							onClick: () => setTabs(1),
+							_active: {
+								bg: 'indigoAlpha.200',
+							},
+							_hover: {
+								bg: 'indigoAlpha.200',
+							},
+							color: 'indigo.200',
 							icon: <FiLayers/>
 						}}
 					/>
 				</HStack>
 			</WidgetHeader>
-			<Divider/>
 			<WidgetBody>
 				<FormContainer>
 					<Inputs refs={titleRef} pHolder={'title'} />
-					<TextAreas refs={notesRef} pHolder={'notes'} />
+					<TextAreas refs={notesRef} pHolder={'notes'}  />
 				</FormContainer>
 			</WidgetBody>
-			<Divider/>
 			<WidgetFooter>
-				<Buttons
-					custom={{
-						onClick: handleSubmit,
-						bg: 'indigo.200',
-						rounded: 'full',
-						px: 16,
-						py: 4
-					}}
-				>create</Buttons>
+				<HStack justifyContent={'center'}>
+					<Buttons
+						custom={{
+							onClick: handleSubmit,
+							bg: 'indigo.200',
+							color: 'white',
+							rounded: 'md',
+							px: 9,
+							py: 4
+						}}
+					>create</Buttons>
+				</HStack>
 			</WidgetFooter>
-		</>
+		</VStack>
 	);
 };
 

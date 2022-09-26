4d7dd6d86d51a790681ce7a149cb4ce1689c921b
i-one7 <ridhoang93@gmail.com>
2022-09-27 04:32:56 +0700

    change the color arrangement of the buttons

--- src/layouts/content/view.content.jsx
+++ src/layouts/content/view.content.jsx
@@ -1,8 +1,8 @@
 import { Editable, EditableInput, EditableTextarea, EditablePreview, Heading, Text, HStack, Divider, VStack } from '@chakra-ui/react';
 import { WidgetBody, WidgetHeader, WidgetFooter } from '../../components/widget.comp';
-import { Buttons, IButtons } from '../../components/button.comp';
+import { IButtons, Buttons } from '../../components/button.comp';
 import { Inputs, TextAreas } from '../../components/form.comp';
-import { FiLayers, FiEdit, FiX } from 'react-icons/fi';
+import { FiLayers, FiEdit, FiX, FiCheck } from 'react-icons/fi';
 import { EventContext } from '../../context/event.context';
 import { useContext } from 'react';
 
@@ -10,7 +10,7 @@ const Viewtask = () => {
 	const { setTabs, defaultValue, handleValue, isEdit, setIsEdit, handleUpdate } = useContext(EventContext);
 
 	return (
-		<>
+		<VStack w={'full'} gap={3}>
 			<WidgetHeader>
 				<HStack alignItems={'center'} justifyContent={'space-between'}>
 					<Heading size={'md'} fontWeight={'medium'}>task details</Heading>
@@ -18,19 +18,32 @@ const Viewtask = () => {
 						<IButtons
 							custom={{
 								onClick: () => setIsEdit(!isEdit),
-								icon: isEdit ? <FiX/> : <FiEdit/>,
+								_active: {
+									bg: 'violetAlpha.200',
+								},
+								_hover: {
+									bg: 'violetAlpha.200',
+								},
+								color: 'violet.200',
+								icon: isEdit ? <FiX /> : <FiEdit />,
 							}}
 						/>
 						<IButtons
 							custom={{
 								onClick: () => setTabs(1),
+								_active: {
+									bg: 'indigoAlpha.200',
+								},
+								_hover: {
+									bg: 'indigoAlpha.200',
+								},
+								color: 'indigo.200',
 								icon: <FiLayers />,
 							}}
 						/>
 					</HStack>
 				</HStack>
 			</WidgetHeader>
-			<Divider/>
 			<WidgetBody>
 				<VStack gap={2}>
 					<Editable isPreviewFocusable={isEdit} selectAllOnFocus={false} defaultValue={defaultValue.title} value={`${defaultValue.title}`} onChange={(e) => handleValue('title', e)} placeholder={'enter new title'} width={'full'}>
@@ -39,27 +52,25 @@ const Viewtask = () => {
 					</Editable>
 					<Editable isPreviewFocusable={isEdit} selectAllOnFocus={false} defaultValue={defaultValue.notes} value={`${defaultValue.notes}`} onChange={(e) => handleValue('notes', e)} placeholder={'enter new notes'} width={'full'}>
 						<EditablePreview as={Text} h={'11rem'} />
-						<TextAreas as={EditableTextarea} onFocus={true} />
+						<TextAreas as={EditableTextarea} focus={true} />
 					</Editable>
 				</VStack>
 			</WidgetBody>
-			<Divider />
 			<WidgetFooter>
-				{
-					isEdit && (
-						<Buttons
-							custom={{
-								onClick: handleUpdate,
-								bg: 'indigo.200',
-								rounded: 'full',
-								px: 16,
-								py: 4
-							}}
-						>update</Buttons>
-					)
-				}
+				<HStack justifyContent={'center'}>
+					<Buttons
+						custom={{
+							onClick: handleUpdate,
+							disabled: !isEdit,
+							bg: 'indigo.200',
+							color: 'white',
+							rounded: 'md',
+							px: 9,
+							py: 4
+						}}>update</Buttons>
+				</HStack>
 			</WidgetFooter>
-		</>
+		</VStack>
 	);
 };
 

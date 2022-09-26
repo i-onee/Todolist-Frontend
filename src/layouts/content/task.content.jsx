742e32d0eedd6c28ee8806dd6a5d2f82dcafbabc
i-one7 <ridhoang93@gmail.com>
2022-09-27 04:31:30 +0700

    add condition when activity is empty

--- src/layouts/content/task.content.jsx
+++ src/layouts/content/task.content.jsx
@@ -1,7 +1,7 @@
 import { WidgetBody, WidgetHeader, WidgetFooter } from '../../components/widget.comp';
-import { Heading, HStack, VStack, Divider } from '@chakra-ui/react';
 import { FiCheckCircle, FiCircle, FiPlus } from 'react-icons/fi';
 import { EventContext } from '../../context/event.context';
+import { Heading, HStack, VStack, Text } from '@chakra-ui/react';
 import { DataContext } from '../../context/data.context';
 import { IButtons } from '../../components/button.comp';
 import Lists from '../../components/list.comp';
@@ -12,42 +12,46 @@ const Tasklist = () => {
 	const { setTabs, getValue } = useContext(EventContext);
 
 	return (
-		<>
+		<VStack w={'full'} gap={3}>
 			<WidgetHeader>
 				<HStack alignItems={'center'} justifyContent={'space-between'}>
 					<Heading size={'md'} fontWeight={'medium'}>task list</Heading>
 					<IButtons
 						custom={{
 							onClick: () => setTabs(2),
+							_active: {
+								bg: 'indigoAlpha.200',
+							},
+							_hover: {
+								bg: 'indigoAlpha.200',
+							},
+							color: 'indigo.200',
 							icon: <FiPlus/>
 						}}
 					/>
 				</HStack>
 			</WidgetHeader>
-			<Divider/>
 			<WidgetBody>
-				<VStack gap={1}>
+				<VStack gap={2} justifyContent={dataDB.data.length === 0 && 'center'} h={'full'}>
 					{
-						dataDB.data.map((v, i) => {
-							const listProp = {
-								icheck  : v.complete ? <FiCheckCircle /> : <FiCircle />,
-								deletes : () => dataServices('delete', { _id: v._id }),
-								check   : () => dataServices('check', { _id: v._id }),
-								show    : getValue.bind(this, v),
-								checked : v.complete && 's',
-								title   : v.title,
-								key     : i
-							}
-							return <Lists {...listProp}/>
-						})
+						dataDB.data.length !== 0 ? (
+							dataDB.data.map((items, i) => {
+								const listProp = {
+									icheck  : items.complete ? <FiCheckCircle /> : <FiCircle />,
+									deletes : () => dataServices('delete', { _id: items._id }),
+									check   : () => dataServices('check', { _id: items._id }),
+									show    : getValue.bind(this, items),
+									checked : items.complete && 's',
+									title   : items.title,
+									key     : i
+								}
+								return <Lists {...listProp}/>
+							})
+						) : <Heading opacity={0.2} size={'xl'} fontWeight={'medium'}>no task :)</Heading>
 					}
 				</VStack>
 			</WidgetBody>
-			<Divider />
-			<WidgetFooter>
-				FOOTER
-			</WidgetFooter>
-		</>
+		</VStack>
 	);
 };
 

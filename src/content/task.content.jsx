c11078f1429d0956dbfc7382030372b4b2987b0e
i-one7 <ridhoang93@gmail.com>
2022-10-11 03:14:50 +0700

    modify widget components for list wrapper

--- src/content/task.content.jsx
+++ src/content/task.content.jsx
@@ -1,17 +1,16 @@
-import { IButtons, Lists, WidgetBody, WidgetFooter, WidgetHeader, Badges, Headings } from '../components/components';
+import { IButtons, Lists, WidgetBody, WidgetFooter, WidgetHeader, Badges, Headings } from '../components/@components';
 import { FiCheckCircle, FiCircle, FiPlus } from 'react-icons/fi';
-import { DataContext, EventContext} from '../context/context';
-import { Flex } from '@chakra-ui/react';
+import { DataContext, EventContext} from '../context/@context';
 import { useContext } from 'react';
 
 const TaskList = () => {
 	const { setTabs, handleEvent } = useContext(EventContext);
 	const { dataDB } = useContext(DataContext);
-	const totalTask = dataDB.data.length
+	const totalTask = dataDB.data.length;
 	return (
 		<>
 			<WidgetHeader>
-				<Headings size={'md'} tTransform={'uppercase'}>task list</Headings>
+				<Headings size={'xl'} tTransform={'uppercase'}>task list</Headings>
 				<IButtons
 					custom={{
 						_active: { bg: 'indigoAlpha.200' },
@@ -22,25 +21,23 @@ const TaskList = () => {
 					}}
 				/>
 			</WidgetHeader>
-			<WidgetBody>
-				<Flex flexDir={'column'} gap={4} alignItems={totalTask === 0 && 'center'} justifyContent={totalTask === 0 && 'center'} h={'full'}>
-					{
-						totalTask !== 0 ? (
-							dataDB.data.map((items, i) => {
-								const listProp = {
-									icheck: items.complete ? <FiCheckCircle /> : <FiCircle />,
-									deletes: () => handleEvent('delete', items),
-									check: () => handleEvent('check', items),
-									show: () => handleEvent('view', items),
-									checked: items.complete && 's',
-									title: items.title,
-									key: i
-								}
-								return <Lists {...listProp}/>
-							})
-						) : <Headings size={'xl'} align={'center'} opacity={0.1}>no task :)</Headings>
-					}
-				</Flex>
+			<WidgetBody sx={{ overflow: 'auto', display: 'flex', flexDir: 'column', gap: 4, justifyContent: `${totalTask === 0 && 'center'}`, alignItems: `${totalTask === 0 && 'center'}`}}>
+				{
+					totalTask !== 0 ? (
+						dataDB.data.map((items, i) => {
+							const listProp = {
+								icheck: items.complete ? <FiCheckCircle /> : <FiCircle />,
+								deletes: () => handleEvent('delete', items),
+								check: () => handleEvent('check', items),
+								show: () => handleEvent('view', items),
+								checked: items.complete && 's',
+								title: items.title,
+								key: i
+							}
+							return <Lists {...listProp}/>
+						})
+					) : <Headings size={'3rem'} align={'center'} opacity={0.1}>no task :)</Headings>
+				}
 			</WidgetBody>
 			<WidgetFooter>
 				<Badges>{ totalTask } task left</Badges>

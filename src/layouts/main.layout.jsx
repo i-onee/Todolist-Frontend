451aa422956bb00cdd76b1be49e5093d0e812733
i-one7 <ridhoang93@gmail.com>
2022-10-11 03:25:17 +0700

    change import method and modify some sections

--- src/layouts/main.layout.jsx
+++ src/layouts/main.layout.jsx
@@ -1,19 +1,19 @@
-import { TabPanels, TabPanel, Tabs, useColorModeValue, Flex } from '@chakra-ui/react';
-import { CreateTask, TaskList, ViewTask, EditTask } from '../content/content';
-import { Sections, Widget, Navbars } from '../components/components';
-import { EventContext } from '../context/context';
+import { TabPanels, TabPanel, Tabs, Flex } from '@chakra-ui/react';
+import { CreateTask, TaskList, ViewTask, EditTask } from '../content/@content';
+import { Sections, Widget, Navbars } from '../components/@components';
+import { EventContext } from '../context/@context';
 import { useContext } from 'react';
 
 const MainLayout = () => {
 	const { tabs } = useContext(EventContext);
 	return (
 		<>
-			<Sections custom={{ color: useColorModeValue('gray.800', 'white'), bgGradient: 'linear(to-r, violet.300, indigo.300)', h: '40%' }} />
-			<Sections custom={{ position: 'absolute', w: 'full' }}>
-				<Flex flexDir={'column'} gap={2}>
+			<Sections sx={{ bgGradient: 'linear(to-r, violet.300, indigo.300)', h: '40%', maxW: 'container.full' }} />
+			<Sections sx={{ maxW: 'container.sm', display: 'inline-flex', position: 'absolute', inset: 0 }}>
+				<Flex w={'full'} gap={4} flexDir={'column'} my={'auto'}>
 					<Navbars/>
 					<Widget>
-						<Tabs index={tabs}>
+						<Tabs variant={'unstyled'} index={tabs}>
 							<TabPanels>
 								<TabPanel> <EditTask/> </TabPanel>
 								<TabPanel> <ViewTask/> </TabPanel>
@@ -24,7 +24,7 @@ const MainLayout = () => {
 					</Widget>
 				</Flex>
 			</Sections>
-			<Sections custom={{ h: '60%' }} />
+			<Sections sx={{ h: '60%', maxW: 'container.full' }} />
 		</>
 	);
 };

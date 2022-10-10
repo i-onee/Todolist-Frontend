563fc7db3cd9156f501a1bdf31c8105389a79e45
i-one7 <ridhoang93@gmail.com>
2022-10-11 03:09:34 +0700

    modify some components for create section

--- src/content/create.content.jsx
+++ src/content/create.content.jsx
@@ -1,14 +1,15 @@
-import { Buttons, IButtons, WidgetBody, WidgetHeader, WidgetFooter, FormContainer, Inputs, TextAreas, Headings } from '../components/components';
-import { EventContext } from '../context/context';
+import { Buttons, IButtons, WidgetBody, WidgetHeader, WidgetFooter, FormContainer, Inputs, TextAreas, Headings } from '../components/@components';
+import { EventContext } from '../context/@context';
 import { FiLayers } from 'react-icons/fi';
 import { useContext } from 'react';
+import { Divider } from '@chakra-ui/react';
 
 const CreateTask = () => {
 	const { setTabs, handleEvent, userRefs } = useContext(EventContext);
 	return (
 		<>
 			<WidgetHeader>
-				<Headings size={'md'} tTransform={'uppercase'}>create task</Headings>
+				<Headings size={'xl'} tTransform={'uppercase'}>create task</Headings>
 				<IButtons
 					custom={{
 						_active: {bg: 'indigoAlpha.200'},
@@ -21,7 +22,8 @@ const CreateTask = () => {
 			</WidgetHeader>
 			<WidgetBody>
 				<FormContainer>
-					<Inputs id={'ctitle'} refs={ ref => userRefs.current.ctitle = ref } pHolder={'title'} />
+					<Inputs id={'ctitle'} refs={ref => userRefs.current.ctitle = ref} pHolder={'title'} />
+					<Divider mt={0} mb={4}/>
 					<TextAreas id={'cnotes'} refs={ ref => userRefs.current.cnotes = ref } pHolder={'notes'}  />
 				</FormContainer>
 			</WidgetBody>
